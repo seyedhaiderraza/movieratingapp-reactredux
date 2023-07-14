f01cc9bbcb55305fe62029bc05589d57f686780c
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-14 19:07:21 +0530

    header - text-logo and image||formatting --header-flex,bg-color -- logo-size,color -- image width,height,radius

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import user from '../../images/user.png'
+import './Header.scss';
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+   
+      <div className="header">
+          <Link to="/">
+            <div className="logo">Movie App</div>
+          </Link>
+          <div className="user-image">
+            <img src={user} alt="user pic" />
+          </div>
+      </div>
+      
   )
 }
 
