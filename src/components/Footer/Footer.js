8b5dfeee0060864f64ed72997f12738d7b1c2fcd
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-15 11:29:43 +0530

    footer content|| home movie fetch api call in useeffect

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './Footer.scss'
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className="footer">
+      <div>Movie App</div>
+      <div>@2023 Copyright movie ratings, inc affiliates</div>
     </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIkey } from "../../common/apis/MovieApiKey";
 const Home = () => {
+  useEffect(() => {
+    console.log(APIkey);
+    const fetchmovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIkey}&s=movie&type=movie`)
+        .catch((err) => {
+          console.log("Error occured in movies api fetch");
+        });
+        console.log(response)
+    };
+    fetchmovies();
+  }, []);
+
   return (
     <div>
-      Home
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
